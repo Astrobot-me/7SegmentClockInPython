ea41069543bf3be3fb4b1d511ad5bbc93606db09
Astro_bot <autobot268@gmail.com>
2023-09-30 21:10:59 +0530

    7 Segment Display in Pyhton Inittial Commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import datetime
+import time
+import os
+
+one = """
+
+   |
+
+   |
+
+"""
+
+two = """
+ -- 
+   |
+ --
+|   
+ --
+"""
+
+
+three = """
+-- 
+  |
+--
+  |  
+-- 
+"""
+
+four = """
+|  |
+ --
+   |  
+"""
+
+
+# y = str(x)
+# print(type(x))
+# z = str(datetime.datetime.now())
+# print("__\t__")
+# print(y[0:4])
+
+# pr 
+
+print(one, end=" ")
+print(two, end=" ")
+print(three, end=" ")
+print(four, end=" ")
+
+# while True:
+#     x = datetime.datetime.now()
+#     curr_time = x.strftime("%H:%M:%S %p")
+#     print(curr_time)
+#     time.sleep(1)
+#     os.system('CLS')
+    
+
+
